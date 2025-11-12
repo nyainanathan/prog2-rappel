006ce911623ff0e1455b05a4b1dc981fa1b7b794
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-11-12 14:34:45 +0300

    Feat: tutor, class and optional attribute of student

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -2,9 +2,16 @@ import java.time.LocalDate;
 
 public class Student extends Person{
     private String group;
+    private Tutor tutor;
 
     public Student(int id, String firstName, String lastName, LocalDate dateOfBirth, String email, String phoneNumber, String group) {
         super(id, firstName, lastName, dateOfBirth, email, phoneNumber);
         this.group = group;
+    }
+
+    public Student(int id, String firstName, String lastName, LocalDate dateOfBirth, String email, String phoneNumber, String group, Tutor tutor) {
+        super(id, firstName, lastName, dateOfBirth, email, phoneNumber);
+        this.group = group;
+        this.tutor = tutor;
     }
 }
